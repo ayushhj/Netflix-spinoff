a290dd4b9bbbcd4d10b5a6dc9e87486b77e7649a
ayushhj <ayushjoshi494@gmail.com>
2024-03-10 01:34:37 +0530

    minor fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import {onAuthStateChanged } from "firebase/auth";
 import { useDispatch } from 'react-redux'
 import addUser, { removeUser } from "../utils/userSlice"
+import { LOGO, USER_AVATAR } from '../utils/constant';
 
 
 const Header = () => {
@@ -23,7 +24,7 @@ const user = useSelector((store)=>store.user)
 
 
   useEffect(()=>{
-    onAuthStateChanged(auth, (user) => {
+   const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const {uid,email,displayname  } = user;
        //dispatch(addUser({uid:uid,email:email}))
@@ -31,9 +32,11 @@ const user = useSelector((store)=>store.user)
      navigate("/browse")
       } else {
      //dispatch(removeUser())
-      navigate("/")
+      navigate("/") 
       }
     });
+    //Unsusbcribe when component unmounts
+    return() => unsubscribe()
   },[])
   
 
@@ -41,9 +44,9 @@ const user = useSelector((store)=>store.user)
   return (
     <div className='absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between '   >   
         <img className='w-44'
-         src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='Logo'  />
+         src={LOGO} alt='Logo'  />
 <div className=' flex p-2'  >
-  <img className='w-12 h-12' src="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e" />
+  <img className='w-12 h-12' src={USER_AVATAR} />
   <button onClick={handleSignOut}  className='font-bold text-white' >(SignOut)</button>
 </div>
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import { checkValidData } from '../utils/validate';
 import {createUserWithEmailAndPassword , signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from "../utils/firebase" 
+import { BG_IMG } from '../utils/constant';
 
 
 export const Login = () => {
@@ -64,7 +65,7 @@ const handleButtonClick = ()=>{
     <div>
     <Header/>
     <div className='absolute' >
-    <img src='https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg ' alt='bg-img' />
+    <img src={BG_IMG} alt='bg-img' />
     </div>
     <form onClick={(e)=>e.preventDefault()}  className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'   >
         <h1 className='text-3xl font-bold py-4' >{isSignIn?"Sign In ":"Sign Up" }</h1>
